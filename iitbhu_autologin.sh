eb938759dc3680e242301e762e31f19ffdf4e865
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2022-07-30 18:36:07 +0530

    Add login script

---
+++ iitbhu_autologin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PASS=$(cat /home/gandharv/Scripts/iitbhu_wifi_pass.txt)
+ROLL='20124018'
+LOGIN_LINK='http://192.168.249.1:1000/login?'
+MAGIC=$(curl --silent -X GET http://192.168.249.1:1000/login? 2>&1 | grep -E "magic" | awk -F'"' '{print $6}')
+
+LOGOUT_LINK=$(curl --silent -d "4Tredir=${LOGIN_LINK}&magic=${MAGIC}&=&username=${ROLL}&password=${PASS}" -X POST http://192.168.249.1:1000 2>&1)
+LOGOUT_LINK=$(echo -n $LOGOUT_LINK | awk -F'"' '{print $2}' | sed "s/keepalive/logout/")
+
+echo $LOGOUT_LINK > /home/gandharv/Scripts/iitbhu_wifi_logout_link.txt
+
