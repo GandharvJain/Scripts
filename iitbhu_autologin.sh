c1bba2e846f11c4b1c3c1546b3d2afc6fed618e2
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2022-07-31 16:45:50 +0530

    Add regular relogin to reset timer

--- iitbhu_autologin.sh
+++ iitbhu_autologin.sh
@@ -8,17 +8,21 @@ if [ $retVal -eq 28 ]; then
 	exit $retVal
 fi
 
-# Logging out
-curl http://192.168.249.1:1000/logout?
-
 # Setting up params
 PASS=$(cat /home/gandharv/Scripts/iitbhu_wifi_pass.txt)
 ROLL='20124018'
 LOGIN_LINK='http://192.168.249.1:1000/login?'
 MAGIC=$(curl --silent -X GET http://192.168.249.1:1000/login? 2>&1 | grep -E "magic" | awk -F'"' '{print $6}')
 
-# Logging in
-CURL_OUTPUT=$(curl --silent -d "4Tredir=${LOGIN_LINK}&magic=${MAGIC}&=&username=${ROLL}&password=${PASS}" -X POST http://192.168.249.1:1000 2>&1)
+while true; do
+	# Logging out
+	curl http://192.168.249.1:1000/logout?
+	
+	# Logging in
+	CURL_OUTPUT=$(curl --silent -d "4Tredir=${LOGIN_LINK}&magic=${MAGIC}&=&username=${ROLL}&password=${PASS}" -X POST http://192.168.249.1:1000 2>&1)
+
+	sleep 20
+done
 
 # Below lines are not needed as the link http://192.168.249.1:1000/logout? logs you out
 # LOGOUT_LINK=$(echo -n $CURL_OUTPUT | awk -F'"' '{print $2}' | sed "s/keepalive/logout/")
