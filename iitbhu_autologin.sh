ed4c9bfedeb2a81d4c1d02fe43b57aa3d980533a
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2022-07-31 17:11:55 +0530

    Add logging

--- iitbhu_autologin.sh
+++ iitbhu_autologin.sh
@@ -2,8 +2,7 @@
 
 # Checking if connected to IIT (BHU)
 curl --connect-timeout 5 -I http://192.168.249.1:1000/login?
-retVal=$?
-if [ $retVal -eq 28 ]; then
+if [ $? -eq 28 ]; then
 	echo "Not connected to the wifi IIT(BHU)!"
 	exit $retVal
 fi
@@ -14,14 +13,23 @@ ROLL='20124018'
 LOGIN_LINK='http://192.168.249.1:1000/login?'
 
 while true; do
-	# Logging out
+	echo "Logging out..."
 	curl http://192.168.249.1:1000/logout?
 	
 	MAGIC=$(curl --silent -X GET http://192.168.249.1:1000/login? 2>&1 | grep -E "magic" | awk -F'"' '{print $6}')
-	# Logging in
-	CURL_OUTPUT=$(curl --silent -d "4Tredir=${LOGIN_LINK}&magic=${MAGIC}&=&username=${ROLL}&password=${PASS}" -X POST http://192.168.249.1:1000 2>&1)
 
-	sleep 20
+	echo "Logging in..."
+	CURL_OUTPUT=$(curl --silent -d "4Tredir=${LOGIN_LINK}&magic=${MAGIC}&=&username=${ROLL}&password=${PASS}" -X POST http://192.168.249.1:1000 2>&1)
+	retVal=$?
+	if [ retVal -eq 0 ]; then
+		echo "Logged in at $(date)!"
+	else
+		echo "Error logging in!"
+		exit retVal
+	fi
+
+	echo "Sleeping..."
+	sleep 7200
 done
 
 # Below lines are not needed as the link http://192.168.249.1:1000/logout? logs you out
