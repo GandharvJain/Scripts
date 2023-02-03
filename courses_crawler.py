8ffc78e170ce7cef8fca8baaeb1e4ac4a1b6a20c
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2023-02-04 02:18:02 +0530

    Single PDF output and Make code parallel

--- courses_crawler.py
+++ courses_crawler.py
@@ -1,6 +1,8 @@
 import requests
+from unidecode import unidecode
 from bs4 import BeautifulSoup
-import pdfkit
+from multiprocessing import Pool
+from fpdf import FPDF
 
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
 
@@ -8,7 +10,7 @@ def pullPage(dept_code, sec_id):
 	page_url = f"https://courses.iitbhu.ac.in/index.php?dept_code={dept_code}&sec_id={sec_id}"
 	rawpage = requests.get(page_url,headers=headers)
 	soup = BeautifulSoup(rawpage.content, 'lxml')
-
+	output = []
 	courses = soup.findAll("table")[1].findAll('tr')[1:]
 	for c in courses:
 		url = str(c).split('\'')[1]
@@ -18,8 +20,24 @@ def pullPage(dept_code, sec_id):
 		name = course_code + ": " + course_name
 		name = ' '.join(name.split()).replace('/', '|')
 
-		pdfkit.from_url(url, name)
-		print(f"Downloaded {name}.pdf")
+		page = requests.get(url,headers=headers)
+		sp = BeautifulSoup(page.content, 'lxml')
+		body = sp.get_text('\n')
+		output.append((name, body))
+		print(f"Downloaded {name}")
+	return output
 
-for i in range(97, 106):
-	pullPage("CSE", str(i))
+inputs = [("CSE", str(i)) for i in range(97, 106)]
+
+outputs = Pool(8).starmap(pullPage, inputs)
+pdf = FPDF()
+pdf.set_font('Arial')
+for output in outputs:
+	for name, body in output:
+		pdf.add_page()
+		pdf.set_font('', 'B', 14)
+		pdf.write(8, name)
+		pdf.set_font('', '', 10)
+		pdf.write(8, '\n\n' + unidecode(body))
+
+pdf.output('CSE_Syllabus_Booklet.pdf', 'F')
