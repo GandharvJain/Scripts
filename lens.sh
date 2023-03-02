82f1c3418882b8935448d706897a74db2fde08a5
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2023-03-02 22:45:18 +0530

    Uncomment line

--- lens.sh
+++ lens.sh
@@ -5,7 +5,7 @@ tesseract_lang=eng
 # quick language menu, add more if you need other languages.
 
 SCR_IMG=`mktemp`
-#trap "rm $SCR_IMG*" EXIT
+trap "rm $SCR_IMG*" EXIT
 
 scrot -s $SCR_IMG.png -q 100 -l style=solid,width=3
 # increase image quality with option -q from default 75 to 100
