b67e51fe3a7f2ac32594ea961370ef617bc8a596
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2023-04-07 02:29:03 +0530

    Add option to deduplicate saved tracks, Move numeric constants to start

--- spotifyControlsAPI.py
+++ spotifyControlsAPI.py
@@ -6,6 +6,11 @@ from os import popen
 import sys
 import time
 
+MAX_PLAYLIST_ITEMS = 100
+MAX_SAVED_TRACKS = 20
+MAX_THREADS = 128
+LAST_FETCHED_SLACK_MS = 1000
+
 username = ''
 playlist_id = ''
 
@@ -47,21 +52,32 @@ def getSpotipyInstance():
 		notify(f"Can't get token for {username}")
 		exit(1)
 
-def getAllPlaylistTrackURIs(sp, playlist_id):
-	# Get size of playlist
-	playlist_size = int(sp.playlist(playlist_id, fields='tracks')['tracks']['total'])
+def getAllTrackURIs(sp, isPlaylist, playlist_id=""):
+	if isPlaylist:
+		total_count = int(sp.playlist(playlist_id, fields='tracks')['tracks']['total'])
+		tracks = []
+		start = 0
+		step = MAX_PLAYLIST_ITEMS
+	else:
+		response = sp.current_user_saved_tracks(limit=MAX_SAVED_TRACKS)
+		total_count = response['total']
+		tracks = [track['track']['uri'] for track in response['items']]
+		start = MAX_SAVED_TRACKS
+		step = MAX_SAVED_TRACKS
 
 	# Function executed in parallel
-	def getPlaylistTracks(offset):
-		temp_tracks = sp.playlist_tracks(playlist_id, limit=100, offset=offset)['items']
+	def getTrackURIs(offset):
+		if isPlaylist:
+			temp_tracks = sp.playlist_tracks(playlist_id, 'items', MAX_PLAYLIST_ITEMS, offset)['items']
+		else:
+			temp_tracks = sp.current_user_saved_tracks(MAX_SAVED_TRACKS, offset)['items']
 		return [track['track']['uri'] for track in temp_tracks]
 
 	# Checking if playlist already contains track
-	tracks = list()
-	with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+	with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
 		futures = []
-		offsets = range(0, playlist_size, 100)
-		for fetched_tracks in executor.map(getPlaylistTracks, offsets):
+		offsets = range(start, total_count, step)
+		for fetched_tracks in executor.map(getTrackURIs, offsets):
 			tracks.extend(fetched_tracks)
 	return tracks
 
@@ -82,13 +98,13 @@ def playlistControls(option, force_action=False):
 	fetched_time_ms = int(current_track['timestamp'])
 	song_progress_ms = int(current_track['progress_ms'])
 	time_since_last_fetched_ms = current_time_ms - fetched_time_ms
-	if not force_action and time_since_last_fetched_ms > song_progress_ms + 1000:
+	if not force_action and time_since_last_fetched_ms > song_progress_ms + LAST_FETCHED_SLACK_MS:
 		notify("Fetched old data!", "Aborting..", icon_red_exclaimation)
 		exit(1)
 
 	# Ignore existence of track in playlist if user forces, may cause duplicates
 	if not force_action:
-		playlist_contains_track = current_track_uri in getAllPlaylistTrackURIs(sp, playlist_id)
+		playlist_contains_track = current_track_uri in getAllTrackURIs(sp, True, playlist_id)
 
 	# If option is add-to-playlist
 	if option == 'add-to-playlist':
@@ -136,58 +152,62 @@ def playlistControls(option, force_action=False):
 			message = f"'{current_track_name}' by '{current_track_artists}'"
 			notify(title, message, icon_red_cross)
 
-def removeDuplicates():
+def removeDuplicates(isPlaylist):
 	sp = getSpotipyInstance()
-	indexed_tracks = enumerate(getAllPlaylistTrackURIs(sp, playlist_id))
+	indexed_tracks = enumerate(getAllTrackURIs(sp, isPlaylist, playlist_id))
 	seen = set()
 	duplicates = dict()
 	for index, track in indexed_tracks:
 		duplicates.setdefault(track, []).append(index) if track in seen else seen.add(track)
 
 	tracks_to_remove = [{"uri": track, "positions": indices} for track, indices in duplicates.items()]
-	sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, tracks_to_remove)
 
-	print(*tracks_to_remove, sep='\n')
 	if len(tracks_to_remove) != 0:
+		if isPlaylist:
+			sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, tracks_to_remove)
+		else:
+			sp.current_user_saved_tracks_delete(tracks_to_remove)
 		with open(log_file, 'w') as f:
 			print(*tracks_to_remove, file=f, sep='\n')
+	print(*tracks_to_remove, sep='\n')
 
-	title = "Removed duplicates from playlist" if bool(tracks_to_remove) else "No duplicates"
-	message = f"Check {log_file} for removed tracks:" if bool(tracks_to_remove) else ""
-	notify(title, message, icon_green_tick)
+	tracks_were_removed = bool(tracks_to_remove)
+	title = f"Removed duplicates from {'playlist' if isPlaylist else 'saved tracks'}"
+	title = title if tracks_were_removed else "No duplicates"
+	message = f"See {log_file} for details" if tracks_were_removed else "No tracks removed"
+	notify(title, message, icon_green_tick if tracks_were_removed else icon_red_cross)
 
-def playerControls(action):
+def playerControls(action, force_action=False):
 	sp = getSpotipyInstance()
 	playback_state = sp.current_playback()
 	if playback_state is None:
 		notify("No device playing spotify", "Aborting..", icon_red_exclaimation)
 		exit(1)
 
-	if action == 'next':
-		sp.next_track()
-	elif action == 'previous':
-		sp.previous_track()
+	if action == 'next': sp.next_track()
+	elif action == 'previous': sp.previous_track()
+	elif playback_state['is_playing']: sp.pause_playback()
 	else:
-		if playback_state['is_playing']:
-			sp.pause_playback()
-		else:
+		try: sp.start_playback()
+		except Exception as e1:
 			# In private session 'is_playing' is always False but controls still work
-			try: sp.start_playback()
-			except:
+			if force_action:
 				try: sp.pause_playback()
-				except Exception as e: raise(e)
+				except Exception as e2: raise(e2)
+			else: raise(e1)
 
 def printHelp():
 		print(f'''
-Usage: {args[0]} [options] [actions]
+Usage: {args[0]} [actions] [options]
 Supported actions:
-	play-pause:                toggle playback
-	next:                      go to next song
-	previous:                  go to previous song
-	add-to-playlist:           add current song to playlist
-	remove-from-playlist:      remove current song from playlist
-	remove-duplicates:         remove duplicate songs from playlist
-	help:                      this help message
+	play-pause:                     toggle playback
+	next:                           go to next song
+	previous:                       go to previous song
+	add-to-playlist:                add current song to playlist
+	remove-from-playlist:           remove current song from playlist
+	remove-playlist-duplicates:     remove duplicate songs from playlist
+	remove-saved-duplicates:        remove duplicate songs from saved tracks
+	help:                           this help message
 Supported options:
 	-f:                        force addition to (or removal from) playlist
 	                           (May create duplicates)
@@ -206,8 +226,8 @@ if __name__ == '__main__':
 	if option in ['add-to-playlist', 'remove-from-playlist']:
 		playlistControls(option, force_action)
 	elif option in ['play-pause', 'next', 'previous']:
-		playerControls(option)
-	elif option == 'remove-duplicates':
-		removeDuplicates()
+		playerControls(option, force_action)
+	elif option in ['remove-playlist-duplicates', 'remove-saved-duplicates']:
+		removeDuplicates(option == 'remove-playlist-duplicates')
 	else:
 		printHelp()
