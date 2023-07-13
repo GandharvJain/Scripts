cd036814ba1af09b9a26c05ce8242b17a03bf942
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2023-07-13 12:41:55 +0530

    Switch PDF library

--- courses_crawler.py
+++ courses_crawler.py
@@ -1,19 +1,49 @@
 #!/usr/bin/python3
 import requests
-from unidecode import unidecode
 from bs4 import BeautifulSoup
-from multiprocessing import Pool
-from fpdf import FPDF
+from multiprocessing import Pool, Manager
+import pdfkit as pk
+from pypdf import PdfReader, PdfWriter
+import io
+
+codes = {
+	"APC": [*range(1, 10), 236, *range(248, 252)],
+	"APM": [*range(10, 19), 238],
+	"APP": [*range(19, 28), 235, 245, 246, 247, 252],
+	"BCE": [*range(28, 39), 227],
+	"BME": [*range(39, 50), 228],
+	"CER": [*range(50, 68), 229],
+	"CHE": [*range(68, 78), 230],
+	"CIV": [*range(78, 97), 231],
+	"CSE": [*range(97, 114), 232],
+	"ECE": [*range(114, 124), 233],
+	"EEE": [*range(124, 143), 234],
+	"MEC": [*range(143, 162), 239],
+	"MET": [*range(162, 181), 240],
+	"MIN": [*range(181, 200), 241],
+	"PHE": [*range(200, 216), 242],
+	"SMST": [*range(216, 227), 237],
+	"INST_CRCS": [243],
+	"HSS": [244],
+}
+department_code = "CSE"
+section_ids = codes[department_code]
+
+pdf_name = f'{department_code}_Syllabus_Booklet.pdf'
 
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+
+manager = Manager()
+courses = manager.dict()
 
 def pullPage(dept_code, sec_id):
 	page_url = f"https://courses.iitbhu.ac.in/index.php?dept_code={dept_code}&sec_id={sec_id}"
 	rawpage = requests.get(page_url,headers=headers)
 	soup = BeautifulSoup(rawpage.content, 'lxml')
 	output = []
-	courses = soup.findAll("table")[1].findAll('tr')[1:]
-	for c in courses:
+
+	course_entries = soup.findAll("table")[1].findAll('tr')[1:]
+	for c in course_entries:
 		url = c.find("button")['onclick'].split('\'')[1]
 
 		course_code = c.find("td").text
@@ -21,25 +51,65 @@ def pullPage(dept_code, sec_id):
 		name = course_code + ": " + course_name
 		name = ' '.join(name.split()).replace('/', '|')
 
-		page = requests.get(url,headers=headers)
-		sp = BeautifulSoup(page.content, 'lxml')
-		body = sp.get_text('\n')
-		output.append((name, body))
+		if course_code in courses or course_name in courses:
+			continue
+		courses[course_code] = True
+		courses[course_name] = True
+
+		course_name_html = f"""
+			<!DOCTYPE html>
+			<html lang="en">
+				<head>
+					<style>
+						html, body {{
+							height: 98%;
+							width: 100%;
+						}}
+
+						body {{
+							display: table;
+						}}
+
+						.my-block {{
+							text-align: center;
+							display: table-cell;
+							vertical-align: middle;
+							font-size: 35px;
+							font-weight: bold;
+						}}
+					</style>
+				</head>
+				<body>
+					<div class="my-block">
+					{name}
+					</div>
+				</body>
+			</html>"""
+
+		stream = io.BytesIO(pk.from_string(course_name_html))
+		output.append((course_name, PdfReader(stream)))
+
+		stream = io.BytesIO(pk.from_url(url))
+		output.append((course_code, PdfReader(stream)))
+
 		print(f"Downloaded {name}")
 	return output
 
-# inputs = [("CSE", str(i)) for i in range(97, 106)]
-inputs = [("ECE", str(i)) for i in range(114, 124)]
+inputs = [(department_code, str(i)) for i in section_ids]
 
 outputs = Pool(8).starmap(pullPage, inputs)
-pdf = FPDF()
-pdf.set_font('Arial')
-for output in outputs:
-	for name, body in output:
-		pdf.add_page()
-		pdf.set_font('', 'B', 14)
-		pdf.write(8, name)
-		pdf.set_font('', '', 10)
-		pdf.write(8, '\n\n' + unidecode(body))
 
-pdf.output('CSE_Syllabus_Booklet.pdf', 'F')
+# print(courses)
+print("Writing to PDF...")
+
+writer = PdfWriter()
+for output in outputs:
+	for code, pdf in output:
+		if code in courses:
+			writer.append_pages_from_reader(pdf)
+			del courses[code]
+
+with open(pdf_name, 'wb') as f:
+	writer.write(f)
+
+print(f"Written to {pdf_name}")
