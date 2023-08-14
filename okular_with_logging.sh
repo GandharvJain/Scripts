be72ea512c433c7494099dbf5446fc2fb3c99069
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2023-08-14 22:16:50 +0530

    Improve error handling and efficiency

--- okular_with_logging.sh
+++ okular_with_logging.sh
@@ -6,8 +6,14 @@ xargs -0 -d '\n' -a $okular_config flatpak run org.kde.okular &
 sleep 3
 
 okular_pid=`pgrep -n okular-bin`
-while [[ $okular_pid == `pgrep okular-bin` ]]; do
-	open_files="`lsof -p $okular_pid | grep -o /home.*pdf | sort`"
+process_dir="/proc/$okular_pid"
+cmd_line_file="$process_dir/cmdline"
+open_files_dir="$process_dir/fd"
+cmd_line=`strings $cmd_line_file`
+
+while [[ -d $process_dir ]]; do
+	[[ `strings $cmd_line_file` != $cmd_line ]] && exit
+	open_files="`ls -l1 $open_files_dir | sort | grep -o /home.*pdf`"
 	[[ -n $open_files ]] && echo "$open_files" > $okular_config
 	sleep 300
 done
