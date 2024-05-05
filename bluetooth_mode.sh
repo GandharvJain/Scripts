780c970a308b5071a13186032909abba3546b96e
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2024-05-05 22:55:10 +0530

    Add Pipewire compatibility

--- bluetooth_mode.sh
+++ bluetooth_mode.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+check_str=$(pactl info | grep -i pipewire)
+
+
 for card in $(pactl list cards short | grep bluez_card | cut -f 2); do
-	if [ "$1" = "hsp" ]; then
-		pacmd set-card-profile $card headset_head_unit
+	if [[ -z $check_str ]]; then
+		if [ "$1" = "hsp" ]; then
+			pacmd set-card-profile $card headset_head_unit
+		else
+			pacmd set-card-profile $card a2dp_sink
+		fi
 	else
-		pacmd set-card-profile $card a2dp_sink
+		if [ "$1" = "hsp" ]; then
+			pactl set-card-profile $card headset-head-unit
+		else
+			pactl set-card-profile $card a2dp-sink
+		fi
 	fi
 done
